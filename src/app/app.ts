f48188288c61e6d57b80ad320dbb30aee4f0b664
AritzCiriza <aritzciriza@gmail.com>
2026-06-01 21:32:19 +0200

    Añado etiqueta para ver el trafico del portafolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
@@ -7,6 +9,8 @@ import { ExperienciaComponent } from './components/experiencia/experiencia.compo
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -24,4 +28,14 @@ import { ContactoComponent } from './components/contacto/contacto.component';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-YHTR67FLD0', {
+        page_path: event.urlAfterRedirects
+      });
+    });
+  }
+}
